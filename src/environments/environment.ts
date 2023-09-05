eb5f4cfec0c56135dd354b2b3421e66a15be75c8
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-09-04 23:26:04 -0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false
+};
+
+export const SpotifyConfiguration = {
+  clientId: '8aa61ff0902643febf563faee56d6bd6',
+  aithEndPoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+    "user-library-read",
+    "playlist-read-private",
+    "playlist-read-collaborative"
+  ]
+}
